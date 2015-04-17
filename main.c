ca8255b4e884b3f7efae0623c4102f59fd24e3c5
wanim <wanim@users.noreply.github.com>
2015-04-17 13:43:27 +0300

    fixed func names

--- main.c
+++ main.c
@@ -9,31 +9,31 @@ int main()
 {
     char* pt = (char*)malloc(4096);
     #ifdef STATICALLY_ALLOCATED_HEAP
-    heap_init();
+    timm_init();
     #endif // STATICALLY_ALLOCATED_HEAP
     #ifdef DYNAMICALLY_ALLOCATED_HEAP
-    heap_init(pt, 4096, 8);
+    timm_init(pt, 4096, 8);
     #endif // DYNAMICALLY_ALLOCATED_HEAP
 
 
-    int* my_ptr = (int*)heap_new(12);
+    int* my_ptr = (int*)timm_new(12);
     printf("%pt, %p\n", pt, my_ptr);
     //*my_ptr = 0x11223344;
-    int* my_ptr1 = (int*)heap_new(16);
+    int* my_ptr1 = (int*)timm_new(16);
     //*my_ptr1 = 0x66778899;
-    heap_delete(my_ptr, 12);
-    double* my_ptr2 = (double*)heap_new(40);
+    timm_delete(my_ptr, 12);
+    double* my_ptr2 = (double*)timm_new(40);
 
     print_map();
-    LenT freeBlk =  heap_get_free_blocks();
-    LenT blkCnt = heap_get_blocks_count();
+    LenT freeBlk = timm_get_free_blocks();
+    LenT blkCnt = timm_get_blocks_count();
     printf("\n\n\t%d blocks of %d are free\n\n", freeBlk, blkCnt);
 
-    heap_delete(my_ptr1, 16);
-    heap_delete(my_ptr2, 40);
+    timm_delete(my_ptr1, 16);
+    timm_delete(my_ptr2, 40);
     print_map();
-    freeBlk =  heap_get_free_blocks();
-    blkCnt = heap_get_blocks_count();
+    freeBlk =  timm_get_free_blocks();
+    blkCnt = timm_get_blocks_count();
     printf("\n\n\t%d blocks of %d are free\n\n", freeBlk, blkCnt);
 
 
